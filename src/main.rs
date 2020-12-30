5cf52f309b81cbaada44e44d0e784f1388a1f574
Karl Hallsby <karl@hallsby.com>
2020-12-30 11:47:59 -0600

    Define an enum for possible mathematical operations to perform

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+/* Mathematical operations we can perform.
+ * To make things slightly easier on ourselves, and to leverage the strong
+ * type system that Rust has, we have this enum derive some trait functionality. */
+#[derive(Debug, Eq, PartialEq)]
+enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div
+}
+
 fn main() {
     println!("Hello, world!");
 }
