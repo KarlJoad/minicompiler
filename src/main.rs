0ff75a5c1ce5dcd1665dc94af99b31b936d38121
Karl Hallsby <karl@hallsby.com>
2020-12-30 12:46:37 -0600

    Math numbers, but no number parsing implemented yet

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,11 @@ fn lex(input: &str) -> Result<Vec<Token>, BadInput> {
 	    '(' => result.push(LeftParen),
 	    ')' => result.push(RightParen),
 
+	    // Numbers
+	    '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {
+		todo!("Implement number parsing!")
+	    }
+
 	}
     }
 
